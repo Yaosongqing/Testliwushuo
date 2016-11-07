f8e4e1149f37176de31261457dbc09ff8d57b9f0
ysq <564248651@qq.com>
2016-11-07 13:12:08 +0800

    初始导入

---
+++ app/src/main/java/com/text/ysq/liwushuo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.text.ysq.liwushuo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super .onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        switch (view.getId()){
+            case R.id.main_fragment_content_fl:
+                break;
+        }
+    }
+}
